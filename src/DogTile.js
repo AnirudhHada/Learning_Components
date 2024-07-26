1984a57fbd481270e9422ca05ba4d41840daf22d
AnirudhHada <hadaanirudh10@gmail.com>
2024-07-26 20:43:57 +0530

    made a function

---
+++ src/DogTile.js
@@ -0,0 +1,12 @@
+import './DogTile.css';
+
+function DogTile() {
+	return (
+		<div className="first">
+			<img src="https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg" />
+			<h3>Cute Puppy</h3>
+		</div>
+	);
+}
+
+export default DogTile;
