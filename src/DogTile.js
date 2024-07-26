b05139effed7bc7907a8c1da79aa423044445ca5
AnirudhHada <hadaanirudh10@gmail.com>
2024-07-26 21:00:43 +0530

    updated js file

--- src/DogTile.js
+++ src/DogTile.js
@@ -3,7 +3,7 @@ import './DogTile.css';
 function DogTile() {
 	return (
 		<div className="first">
-			<img src="https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg" />
+			<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoBuMvSuYezLE9rwI-zOJeIOmcIGfDPqOvFA&s" />
 			<h3>Cute Puppy</h3>
 		</div>
 	);
