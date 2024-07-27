bdabc81fbd77418ff42b9fd113a00b006447060a
AnirudhHada <hadaanirudh10@gmail.com>
2024-07-27 08:46:36 +0530

    updated js file

--- src/DogTile.js
+++ src/DogTile.js
@@ -1,10 +1,12 @@
 import './DogTile.css';
+import DogDescription from './DogDescription';
 
 function DogTile() {
 	return (
 		<div className="first">
 			<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoBuMvSuYezLE9rwI-zOJeIOmcIGfDPqOvFA&s" />
 			<h3>Cute Puppy</h3>
+         <DogDescription />
 		</div>
 	);
 }
